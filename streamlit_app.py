5263fad04b81a0be98c20652f3763c9ad827e6b7
Khoirun Nisa Alfaini <74775762+khoirunna@users.noreply.github.com>
2025-06-05 00:38:44 +0700

    Update streamlit_app.py
    Comment session = get_active_session()

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ session = cnx.session()
 
 # st.write("You selected:", option)
 
-session = get_active_session()
+# session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
